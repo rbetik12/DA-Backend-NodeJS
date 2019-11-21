190244acb7959c892792caa920b92b50039ab3bd
Gleb Korzan <gleb.korzan@gmail.com>
2019-11-21 23:36:42 +0300

    fix typo

--- app/common/models/message-model.interface.ts
+++ app/common/models/message-model.interface.ts
@@ -3,7 +3,7 @@ export interface MessageModel {
     userID: number;
     sender: string;
     text: string;
-    longitudea: number;
-    altitude: number;
+    latitude: number;
+    longitude: number;
     coefficient?: number;
 }
